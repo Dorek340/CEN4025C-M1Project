f0bc8c4aafaba98ef764effbbb67d251d1377d85
David Singletary <david.singletary@fscj.edu>
2025-01-05 17:42:11 -0500

    spring 25 changes

--- PizzaRestaurant/src/main/java/edu/fscj/cen4025c/pizzarestaurant/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package edu.fscj.cen4025c.pizzarestaurant.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- PizzaRestaurant/src/main/java/edu/fscj/cen4025c/pizzarestaurant/exceptions/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package edu.fscj.cen4025c.pizzarestaurant.exceptions;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
